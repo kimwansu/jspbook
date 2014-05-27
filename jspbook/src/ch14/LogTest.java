be31265a38258722c30b925c90ad2ff5a052ac45
Kim Wansu <wantalia@gmail.com>
2014-05-27 18:25:34 +0900

    14장 작업 시작

---
+++ jspbook/src/ch14/LogTest.java
@@ -0,0 +1,17 @@
+package ch14;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogTest
+{
+    public static void main(String[] args)
+    {
+        String msg = "Hello Log!!";
+        Logger logger = LoggerFactory.getLogger(LogTest.class);
+        logger.info("test log");
+        logger.warn("test log : {}", msg);
+    }
+}
+
+
